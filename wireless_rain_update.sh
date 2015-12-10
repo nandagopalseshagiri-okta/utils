523134bd0dd4d9a0bf10c27b54ae92e97f8fd8a4
nanda <nandagopal.seshagiri@okta.com>
2015-12-10 12:02:52 -0800

    Start dnsserver with update wireless IP
    - change host file to point rain to wireless IP
    - start dnsmasq
    - wait
    - revert the host file back
    - dnsmasq keeps a caches the host entry

--- wireless_rain_update.sh
+++ wireless_rain_update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function brew_package_location() {
+	echo `brew info $1 | grep -m 1 \/usr | cut -d ' ' -f1`
+}
+
 wireless_ip=$(ifconfig en0 | grep "inet " | cut -d' ' -f2)
 echo "wireless_ip = $wireless_ip"
 
@@ -10,8 +14,23 @@ echo $wireless_ip rain-admin.okta1.com >> $temp_host_file
 
 echo $temp_host_file
 
-sudo rm /etc/hosts.old
+dnsmasq_pid=$(sudo ps -A | grep [d]nsmasq | cut -d ' ' -f1)
+if [  "$dnsmasq_pid" != "" ]
+	then
+	echo "dnsmasq is running. pid = $dnsmasq_pid. Stopping it first"
+	sudo kill -9 $dnsmasq_pid
+fi
+
+sudo rm /etc/hosts.old 2> /dev/null > /dev/null
 sudo cp /etc/hosts /etc/hosts.old
 sudo mv $temp_host_file /etc/hosts
+
+dnsmasq_loc=$(brew_package_location dnsmasq)
+sudo $dnsmasq_loc/sbin/dnsmasq
+
+echo "Sleeping for few minutes to allow dnsmasq to pick up host file change before reverting to old ..."
+sleep 3
+
+sudo mv /etc/hosts.old /etc/hosts
 
 # # change /etc/hosts to make rain.okta1.com and rain-admin.okta1.com point to the non-loopback IP
