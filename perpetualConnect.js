ae119b0bd870f476971af4fc1a950bb3fee720cf
nanda <nandagopal.seshagiri@okta.com>
2018-01-29 16:25:57 -0800

    Exchange for - two incoming connections - two outgoing connnection

---
+++ perpetualConnect.js
@@ -0,0 +1,49 @@
+var program = require('commander');
+program
+  .option('-l, --left <host:port>', 'left host to pipe to')
+  .option('-r, --right <host:port>', 'right host to pipe to')
+  .parse(process.argv);
+
+function validated(argName) {
+  var item = program[argName] != null ? program[argName].toString().split(':') : [];
+  if (item.length != 2 || parseInt(item[1]) <= 0) {
+    console.log('please specify a ' + argName + ' host to pipe to');
+    process.exit(1);
+  }
+  return item;
+}
+
+var right = validated("right");
+var left = validated("left");
+
+var net = require('net');
+
+function connect(remote, ondata, onconnect) {
+  var rm = new net.Socket();
+	rm.on('error', function (e) {
+		console.log('error connecting to remote. ' + e.toString());
+	});
+	rm.connect(parseInt(remote[1]), remote[0],
+		function () {
+			if (onconnect) { onconnect(rm) };
+    });
+  rm.on('data', function(data) {
+    if (ondata) {
+      console.log('Reacting to data arrival');
+      ondata(data, rm) };
+  });
+}
+
+function connectRight() {
+  connect(right, function (data, rightConnection) {
+    rightConnection.removeAllListeners('data');
+    connect(left, null, function (leftConnection) {
+      leftConnection.write(data);
+      rightConnection.pipe(leftConnection);
+      leftConnection.pipe(rightConnection);
+    });
+    setImmediate(connectRight);
+  });
+}
+
+connectRight();
---
+++ rpipe.js
@@ -0,0 +1,79 @@
+var program = require('commander');
+program
+	.option('-l, --lport <port>', 'left incoming',parseInt)
+	.option('-r, --rport <port>', 'right incoming', parseInt)
+	.parse(process.argv);
+
+if (program.lport === null || program.rport === null || !program.lport || !program.rport) {
+	console.log('please specify valid left and right incoming ports');
+	process.exit(1);
+}
+
+var net = require('net');
+
+function leftPort(port) {
+  var queue = [];
+  var server = net.createServer(function(lc) {
+    queue.push(lc);
+    lc.on('end', function() {
+      console.log('[LEFT] client disconnected');
+    });
+    lc.on('error', function (e) {
+      console.log('[LEFT] error occured on incoming end of pipe. Info = ' + e.toString());
+    });
+    lc.on('timeout', function (e) {
+      console.log('[LEFT] timeout trying to connect to remote. ' + e.toString());
+    });
+  });
+
+  server.on('error', function (e) {
+    console.log('[LEFT] server error - ' + e.toString());
+  });
+  
+  server.listen(port, function() {
+    console.log('[LEFT] server bound');
+  });
+  return queue;
+}
+
+var MAX_WAIT_COUNT = 5;
+
+function pickAndJoinConnectionFromQueueOrWait(queue, rc, count) {
+  if (queue.length <= 0) {
+    if (count >= MAX_WAIT_COUNT) {
+      console.log('[RIGHT] reached/exceeded max wait count waiting for left connection. waitCount = ' + count);
+    }
+    setTimeout(pickAndJoinConnectionFromQueueOrWait, 1000, queue, rc, ++count);
+    return;
+  }
+  var lc = queue.pop();
+  lc.pipe(rc);
+  rc.pipe(lc);
+}
+
+function rightPort(port, queue) {
+  var server = net.createServer(function(rc) {
+    rc.on('end', function() {
+      console.log('[RIGHT] client disconnected');
+    });
+    rc.on('error', function (e) {
+      console.log('[RIGHT] error occured on incoming end of pipe. Info = ' + e.toString());
+    });
+    rc.on('timeout', function (e) {
+      console.log('[RIGHT] timeout trying to connect to remote. ' + e.toString());
+    });
+
+    pickAndJoinConnectionFromQueueOrWait(queue, rc, 0);
+  });
+
+  server.on('error', function (e) {
+    console.log('[RIGHT] server error - ' + e.toString());
+  });
+  
+  server.listen(port, function() {
+    console.log('[RIGHT] server bound');
+  });
+}
+
+var queue = leftPort(program.lport);
+rightPort(program.rport, queue);
