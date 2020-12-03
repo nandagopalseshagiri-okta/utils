7c2e447c399bf6fe859d04cbb7b76d66a0cbcab4
Nandagopal Seshagiri <nandagopal.seshagiri@okta.com>
2020-12-02 19:27:43 -0800

    Code for complete flow - Ability to read from chain store - checking for duplicate chains - ability to write to chain store (using sha256 of the entire   chain as the file name)

--- certChainManager/src/com/okta/certchainmgr/CertChainManager.java
+++ certChainManager/src/com/okta/certchainmgr/CertChainManager.java
@@ -1,11 +1,20 @@
 package com.okta.certchainmgr;
 
+import sun.security.provider.X509Factory;
+
 import javax.xml.bind.DatatypeConverter;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateEncodingException;
@@ -13,26 +22,57 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 public class CertChainManager {
+    public static String CHAIN_FILE_EXT = ".cchain";
     private Map<String, List<X509Certificate>> certs = new HashMap<>();
     private Map<String, List<X509Certificate>> rootCerts = new HashMap<>();
     private Map<String, List<X509Certificate>> byIssuers = new HashMap<>();
     private Map<String, X509Certificate> certByHashes = new HashMap<>();
 
+    public class CertChain {
+        private List<X509Certificate> chain;
+        private String sha256;
+
+        public CertChain(List<X509Certificate> chain) {
+            this.chain = chain;
+        }
+
+        public CertChain(List<X509Certificate> chain, String sha256) {
+            this.chain = chain;
+            this.sha256 = sha256;
+        }
+
+        public List<X509Certificate> getChain() {
+            return chain;
+        }
+
+        public String getSha256() {
+            return sha256;
+        }
+
+        public void completeChain() {
+            this.sha256 = sha256(this.chain);
+        }
+    }
+
     public class LoadResult {
         public List<String> chainHeads;
         public int ingnoredLeafCertCount;
         public int danglingChainsCount;
+        public CertChainBuilder certChainBuilder;
 
-        public LoadResult(List<String> heads, int leavesIgnored, int danglingChainsCount) {
+        public LoadResult(List<String> heads, int leavesIgnored, int danglingChainsCount, CertChainBuilder certChainBuilder) {
             chainHeads = heads;
             ingnoredLeafCertCount = leavesIgnored;
             this.danglingChainsCount = danglingChainsCount;
+            this.certChainBuilder = certChainBuilder;
         }
 
         public String toString() {
@@ -47,7 +87,7 @@ public class CertChainManager {
     }
 
     class CertChainBuilder {
-        private List<List<X509Certificate>> fullChains = new ArrayList<>();
+        private List<CertChain> fullChains = new ArrayList<>();
         private List<List<X509Certificate>> chains = new ArrayList<>();
         private Map<String, List<List<X509Certificate>>> chainTopOpenEnd = new HashMap<>();
         private Map<String, List<List<X509Certificate>>> chainBottomOpenEnd = new HashMap<>();
@@ -57,7 +97,7 @@ public class CertChainManager {
             return chains;
         }
 
-        public List<List<X509Certificate>> getFullChains() {
+        public List<CertChain> getFullChains() {
             return fullChains;
         }
 
@@ -93,7 +133,7 @@ public class CertChainManager {
 
         private boolean addToTopIndex(X509Certificate certificate, List<X509Certificate> chain) {
             if (isCertRoot(certificate)) {
-                fullChains.add(chain);
+                fullChains.add(new CertChain(chain));
                 return true;
             }
             String key = certificate.getIssuerDN().toString();
@@ -130,27 +170,71 @@ public class CertChainManager {
         public void incrementIgnoredLeaves(int ignoredLeaves) {
             this.ignoredLeaves += ignoredLeaves;
         }
+
+        public void done() {
+            fullChains.stream().forEach(certChain -> certChain.completeChain());
+        }
     }
 
     public LoadResult loadCertChains(String filePath) {
         FileInputStream fileInputStream = null;
         try {
-            new FileInputStream(filePath);
+            fileInputStream = new FileInputStream(filePath);
         } catch (FileNotFoundException e) {
             throw new CCException("File error - " + e.getMessage(), e);
         }
         return loadCertChains(fileInputStream);
     }
 
-    public LoadResult loadCertChains(InputStream inputStream) {
-        CertChainBuilder certChainBuilder = makeOrAddToCertChainBuilder(inputStream, null);
-        List<String> chainHeads = new ArrayList<>();
-        certChainBuilder.getFullChains().stream().forEach((lc) -> {
-            lc.stream().findFirst().map(c -> c.getSubjectDN().toString()).map(s -> chainHeads.add(s));
-        });
-        return new LoadResult(chainHeads, certChainBuilder.getIgnoredLeaves(), certChainBuilder.getChains().size() - certChainBuilder.getFullChains().size());
+    private static CCException handleException(Exception argEx, int count) {
+        try {
+            throw argEx;
+        } catch (CertificateException e) {
+            return new CCException(String.format("Error %1$s loading cert # %$2d", e.getMessage(), count), e);
+        } catch (IOException e) {
+            return new CCException("IO Error " + e.getMessage(), e);
+        } catch (NoSuchAlgorithmException e) {
+            return new CCException("Crypto Algo no available " + e.getMessage(), e);
+        } catch (CCException e) {
+            return e;
+        } catch (Exception e) {
+            return new CCException("Unknown exception", e);
+        }
     }
 
+    /**
+     * Loads certs from a stream and forms as many cert chains as possible and returns a simple printable result
+     * Often calling makeOrAddToCertChainBuilder directly is what most programs would need. This method is just a quick testing
+     * method.
+     * @param inputStream
+     * @return
+     */
+    public LoadResult loadCertChains(InputStream inputStream) {
+        CertChainBuilder certChainBuilder = makeOrAddToCertChainBuilder(inputStream, null);
+        certChainBuilder.done();
+        List<String> chainHeads = new ArrayList<>();
+        certChainBuilder.getFullChains().stream().forEach((lc) -> {
+            lc.getChain().stream().findFirst().map(c -> c.getSubjectDN().toString()).map(s -> chainHeads.add(s));
+        });
+        return new LoadResult(chainHeads, certChainBuilder.getIgnoredLeaves(),
+                certChainBuilder.getChains().size() - certChainBuilder.getFullChains().size(), certChainBuilder);
+    }
+
+    /**
+     * This will create a CertChainBuilder from a stream of certs in no particular order. Passing the returned builder
+     * in subsequent calls will allow building chain or chains using certs from multiple streams. Like root certs could
+     * come from one stream and intermediates from a different stream etc.
+     * Once building is complete you must call done on the builder to indicate that the all well rooted chains are
+     * done - so sha256 for each chain can be computed.
+     *
+     * This method modifies the state of the object - like it stores the certs it has seen - meaning if the same cert is
+     * seen coming through a different stream (in a subsequent call - the duplicate cert would be ignored.
+     * @param inputStream - stream of certs.
+     * @param certChainBuilderOrNull - pass null if you want create a new builder or pass the last returned
+     *                               object to continue to add more chains to the same builder by reading certs from
+     *                               multiple streams
+     * @return
+     */
     public CertChainBuilder makeOrAddToCertChainBuilder(InputStream inputStream, CertChainBuilder certChainBuilderOrNull) {
         int count = 0;
         int ignoredLeaves = 0;
@@ -186,12 +270,163 @@ public class CertChainManager {
 
             certChainBuilder.incrementIgnoredLeaves(ignoredLeaves);
             return certChainBuilder;
-        } catch (CertificateException e) {
-            throw new CCException(String.format("Error %1$s loading cert # %$2d", e.getMessage(), count), e);
-        } catch (IOException e) {
-            throw new CCException("IO Error " + e.getMessage(), e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new CCException("Crypto Algo no available " + e.getMessage(), e);
+        } catch (Exception e) {
+            throw handleException(e, count);
+        }
+    }
+
+    /**
+     * This function assume that the stream contains a well formed chain - that is - first cert should be the root
+     * followed by 1st intermediate and the then second and so on.
+     * This is an optimization - instead of re-building the cert chain using makeOrAddToCertChainBuilder on a well formed
+     * chain, which could be expensive, so this method could be used to create chains from well formed streams.
+     * @param inputStream
+     * @return
+     */
+    public CertChain readChainFromStream(InputStream inputStream) {
+        int count = 0;
+        Charset utf8 = Charset.forName("UTF8");
+        List<X509Certificate> certs = new ArrayList<>();
+        try {
+            MessageDigest finalSha = MessageDigest.getInstance("SHA-256");
+            BufferedInputStream bis = new BufferedInputStream(inputStream);
+            while (bis.available() > 0) {
+                X509Certificate cert = readCert(bis);
+                count++;
+                certs.add(cert);
+                String sha256 = sha256(cert);
+                finalSha.update(sha256.getBytes(utf8));
+            }
+            return new CertChain(certs, DatatypeConverter.printHexBinary(finalSha.digest()).toLowerCase());
+        } catch (Exception e) {
+            throw handleException(e, count);
+        }
+    }
+
+    /**
+     * After reading CertChains from multiples streams this method can be used to de-duplicate the cert chains
+     * and form a map from sha256 of the chain to the chain itself.
+     * Later when new chains are being added this map can be consulted to see if a chain already exists.
+     *
+     * @param chains
+     * @return
+     */
+    public Map<String, CertChain> mapChains(List<CertChain> chains) {
+        Map<String, CertChain> certChainMap = new HashMap<>();
+        chains.stream().forEach(c -> {
+            certChainMap.put(c.getSha256(), c);
+        });
+        return certChainMap;
+    }
+
+    public List<CertChain> readAllWellFormedChainsFromDir(String dirPath) {
+        List<CertChain> allChains = new ArrayList<>();
+        File f = new File(dirPath);
+
+        FilenameFilter filter = (f1, name) -> name.endsWith(CHAIN_FILE_EXT);
+        Arrays.stream(f.list(filter)).map(name -> {
+            File chainFile = new File(f, name);
+            try {
+                return readChainFromStream(new FileInputStream(chainFile.getPath()));
+            } catch (FileNotFoundException e) {
+                return null;
+            }
+        }).filter(Objects::nonNull).forEach(chain -> {
+            allChains.add(chain);
+        });
+
+        return allChains;
+    }
+
+    public class WriteChainResult {
+        private Map<String, Exception> writeException = new HashMap<>();
+        private Map<String, CertChain> duplicateChains = new HashMap<>();
+        private Map<String, String> successfulWrites = new HashMap<>();
+
+        public Map<String, CertChain> getDuplicateChains() {
+            return duplicateChains;
+        }
+
+        public Map<String, Exception> getWriteException() {
+            return writeException;
+        }
+
+        public Map<String, String> getSuccessfulWrites() {
+            return successfulWrites;
+        }
+    }
+
+    public WriteChainResult writeNewChains(CertChainBuilder builder, Map<String, CertChain> existingChainMap, String dirPath) {
+        WriteChainResult writeChainResult = new WriteChainResult();
+        builder.getFullChains().stream().forEach(certChain -> {
+            if (existingChainMap.containsKey(certChain.getSha256())) {
+                writeChainResult.getDuplicateChains().put(certChain.getSha256(), certChain);
+                return;
+            }
+            Exception e = writeChain(certChain, dirPath);
+            if (e != null) {
+                writeChainResult.getWriteException().put(certChain.getSha256(), e);
+            } else {
+                // The value should be the path of the file that contains the chain. For now we can do without it.
+                writeChainResult.getSuccessfulWrites().put(certChain.getSha256(), "");
+            }
+        });
+        return writeChainResult;
+    }
+
+    private Exception writeChain(CertChain certChain, String dirPath) {
+        String certFileName = certChain.getSha256() + CHAIN_FILE_EXT;
+        String tempPath = "/tmp/" + certFileName;
+        try {
+            FileWriter fileWriter = new FileWriter(tempPath);
+            Exception e = certChain.getChain().stream().map(c -> writeCert(c, fileWriter)).filter(Objects::nonNull).findFirst().orElse(null);
+            if (e != null) {
+                tryDeleteFile(tempPath);
+                return e;
+            }
+            fileWriter.flush();
+            fileWriter.close();
+            Files.move(Paths.get(tempPath), Paths.get(dirPath, certFileName));
+            return null;
+        } catch (Exception e) {
+            return e;
+        }
+    }
+
+    private void tryDeleteFile(String path) {
+        try {
+            Files.delete(Paths.get(path));
+        } catch (Exception ignored) {}
+    }
+
+    private Exception writeCert(X509Certificate cert, OutputStreamWriter outputStreamWriter) {
+        try {
+            String certString = DatatypeConverter.printBase64Binary(cert.getEncoded()).replaceAll("(.{64})", "$1\n");
+            if (!certString.endsWith("\n")) {
+                certString = certString + "\n";
+            }
+            outputStreamWriter.write(X509Factory.BEGIN_CERT + "\n");
+            outputStreamWriter.write(certString);
+            outputStreamWriter.write(X509Factory.END_CERT + "\n");
+            outputStreamWriter.flush();
+            return null;
+        } catch (Exception e) {
+            return e;
+        }
+    }
+
+    private static String sha256(List<X509Certificate> certs) {
+        int[] count = {0};
+        try {
+            Charset utf8 = Charset.forName("UTF8");
+            MessageDigest finalSha = MessageDigest.getInstance("SHA-256");
+            certs.stream().forEach(c -> {
+                finalSha.update(sha256_RE(c).getBytes(utf8));
+                count[0]++;
+            });
+            return DatatypeConverter.printHexBinary(finalSha.digest()).toLowerCase();
+        } catch (Exception e) {
+            throw handleException(e, count[0]);
         }
     }
 
@@ -200,6 +435,7 @@ public class CertChainManager {
         map.putIfAbsent(key, new ArrayList<>());
         return map.get(key).add(cert);
     }
+
     private boolean addToMapBySubject(Map<String, List<X509Certificate>> map, X509Certificate cert) {
         return addToMap(map, cert, (c) -> c.getSubjectDN().toString());
     }
@@ -213,10 +449,18 @@ public class CertChainManager {
         return certificate != null && certificate.getBasicConstraints() > 0 && certificate.getSubjectDN().equals(certificate.getIssuerDN());
     }
 
-    private String sha256(X509Certificate cert) throws NoSuchAlgorithmException, CertificateEncodingException {
+    private static String sha256(X509Certificate cert) throws NoSuchAlgorithmException, CertificateEncodingException {
         return DatatypeConverter.printHexBinary(
                 MessageDigest.getInstance("SHA-256").digest(
                         cert.getEncoded())).toLowerCase();
+    }
+
+    private static String sha256_RE(X509Certificate cert) {
+        try {
+            return sha256(cert);
+        } catch (Exception e) {
+            throw handleException(e, 0);
+        }
     }
 
     private void matchAkid() {
--- certChainManager/src/com/okta/certchainmgr/Main.java
+++ certChainManager/src/com/okta/certchainmgr/Main.java
@@ -1,20 +1,36 @@
 package com.okta.certchainmgr;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.StringReader;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            // First we will load all the chains from the cert store directory
+            CertChainManager certChainManager = new CertChainManager();
+            String storeDir = "/Users/nandagopalseshagiri/ccstore-test";
+            Map<String, CertChainManager.CertChain> existingCerts = certChainManager.mapChains(
+                    certChainManager.readAllWellFormedChainsFromDir(storeDir));
+
+            // Now we ask the user to input their set of certs from which we will form chains
             System.out.println("enter the path to certs file");
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(System.in));
 
             String filePath = reader.readLine();
-            CertChainManager certChainManager = new CertChainManager();
-            System.out.println(certChainManager.loadCertChains(filePath));
+            CertChainManager.LoadResult lr = certChainManager.loadCertChains(filePath);
+
+            System.out.println("Completed loading of certs from " + filePath);
+
+            // Certs loaded from user's input are not written to store automatically - we need to do that by calling
+            // writeNewChains and passing the certChainBuilder. Passing existingCerts in not absolutely necessary because
+            // duplicates chains will get the same sha and as the file names in the store will use the sha of the chain - the file would
+            // just fail to be moved to store directory as a file with same sha would already exist.
+            CertChainManager.WriteChainResult wr = certChainManager.writeNewChains(lr.certChainBuilder, existingCerts, storeDir);
+            System.out.println(String.format("%d chains were written", wr.getSuccessfulWrites().size()));
+            System.out.println(String.format("%d chains were duplicate", wr.getDuplicateChains().size()));
+            System.out.println(String.format("%d chains failed to be written", wr.getWriteException().size()));
         } catch (Exception e) {
             System.out.println(e.toString());
         }
